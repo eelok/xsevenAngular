e03957e422604f852cf8f8167e5808f3afa1e19c
wassertim <tim@digitalband.ru>
2018-04-06 22:45:57 +0200

    refs #2 fix the styles

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact',
+  selector: 'app-contact.text-box.text-box__contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  selector: 'app-experience.text-box.text-box__experience',
+  templateUrl: './experience.component.html'
 })
 export class ExperienceComponent implements OnInit {
 
